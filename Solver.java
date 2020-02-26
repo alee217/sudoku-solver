ee9793aabaa79a1853e81ab778102f8db171ea9f
Alex Lee <alee217@uw.edu>
2020-02-26 00:09:39 -0800

    Some fun stuff

---
+++ Solver.java
@@ -0,0 +1,8 @@
+public class Solver {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        int n = 0;
+        int a = 200;
+        System.out.println("Farty McFartface");
+    }
+}
--- Sudoku.java
+++ Sudoku.java
@@ -1,3 +1,17 @@
 public class Sudoku {
-    private 
+    private int[][] grid;
+
+    public Sudoku(int[][] grid) {
+        this.grid = deepCopy(grid);
+    }
+
+    private int[][] deepCopy(int[][] src) {
+        int[][] dst = new int[9][9];
+        for (int i = 0; i < src.length; i++) {
+            for (int j = 0; j < src[0].length; j++) {
+                dst[i][j] = src[i][j];
+            }
+        }
+        return dst;
+    }
 }
