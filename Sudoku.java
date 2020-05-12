0117a62347ccd4ce6368b6de2ad7f879c3cbd910
Alex Lee <alee217@uw.edu>
2020-05-11 21:44:33 -0700

    alksdjfkadsf

--- Sudoku.java
+++ Sudoku.java
@@ -14,4 +14,22 @@ public class Sudoku {
         }
         return dst;
     }
+
+    @Override
+    public String toString() {
+        String ret = "";
+        for (int i = 0; i < this.grid.length; i++) {
+            for (int j = 0; j < this.grid[0].length; j++) {
+                if (j % 3 == 0 && j != 0) {
+                    ret += "| ";
+                }
+                if (i % 3 == 0 && i != 0 && j == 0) {
+                    ret += "---------------------\n";
+                }
+                ret += this.grid[i][j] + " ";
+            }
+            ret += "\n";
+        }
+        return ret;
+    }
 }
