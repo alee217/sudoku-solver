350fbaeb27fa147d9c5e834f61391213aa072122
Alex Lee <alee217@cs.washington.edu>
2020-05-24 11:23:18 -0700

    ajksdhfkjasdf

--- Solver.java
+++ Solver.java
@@ -1,8 +1,17 @@
 public class Solver {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
-        int n = 0;
-        int a = 200;
-        System.out.println("Farty McFartface");
+        int[][] grid = {
+            {3, 5, 0, 0, 0, 0, 0, 8, 0},
+            {0, 0, 0, 0, 5, 8, 9, 0, 2},
+            {0, 0, 0, 0, 0, 7, 0, 0, 0},
+            {0, 1, 0, 0, 0, 5, 0, 4, 0},
+            {0, 0, 0, 7, 0, 0, 0, 9, 0},
+            {0, 0, 9, 0, 1, 2, 0, 0, 7},
+            {0, 0, 0, 0, 0, 0, 0, 0, 3},
+            {6, 0, 0, 2, 0, 0, 0, 0, 0},
+            {0, 0, 2, 3, 0, 0, 1, 0, 8},
+        };
+        Sudoku s = new Sudoku(grid);
+        System.out.println(s.toString());
     }
 }
--- Sudoku.java
+++ Sudoku.java
@@ -1,17 +1,47 @@
 public class Sudoku {
+    public static final int GRID_SIZE = 9;
+
     private int[][] grid;
 
     public Sudoku(int[][] grid) {
         this.grid = deepCopy(grid);
     }
 
+    // Checks whether the current board obeys the One Rule i.e. the rule of the game.
+    public boolean isValid() {
+        return false;
+    }
+
+    // Deep copies a grid.
     private int[][] deepCopy(int[][] src) {
-        int[][] dst = new int[9][9];
-        for (int i = 0; i < src.length; i++) {
-            for (int j = 0; j < src[0].length; j++) {
+        if (src == null || src.length != GRID_SIZE || src[0].length != GRID_SIZE) {
+            throw new IllegalArgumentException();
+        }
+        int[][] dst = new int[GRID_SIZE][GRID_SIZE];
+        for (int i = 0; i < GRID_SIZE; i++) {
+            for (int j = 0; j < GRID_SIZE; j++) {
                 dst[i][j] = src[i][j];
             }
         }
         return dst;
+    }
+
+    @Override
+    public String toString() {
+        String ret = "";
+        for (int i = 0; i < GRID_SIZE; i++) {
+            if (i != 0 && i % 3 == 0) {
+                ret += "-----------------------\n";
+            }
+            ret += " ";
+            for (int j = 0; j < GRID_SIZE; j++) {
+                if (j != 0 && j % 3 == 0) {
+                    ret += "| ";
+                }
+                ret += grid[i][j] + " ";
+            }
+            ret += "\n";
+        }
+        return ret;
     }
 }
